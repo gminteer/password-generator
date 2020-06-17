3f29a756c227f98f90392445e7960a12d2bd9c6d
gminteer <gminteer@gmail.com>
2020-06-16 23:45:48 -0400

    pseudocoded out function, wrote getLength

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment code here
 
-
+var generatePassword() {
+  // get/santize length from user (min:8, max: 128)
+  // get/santize character set choice from user (some combination of uppercase, lowercase, numeric, and special characters)
+  // build a string of all possible password characters from character set choice
+  // randomly pick characters from possible characters string until it's the chosen length
+  // return generated password
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
